08078c451cee57b534b774598d9f15dc40e9e61e
shrugs <mattgcondon@gmail.com>
2023-04-22 21:40:19 -0400

    feat: switch back to puppeteer

--- src/pages/api/image/[hash]/[template].ts
+++ src/pages/api/image/[hash]/[template].ts
@@ -1,7 +1,19 @@
 import { Template } from "@/lib/templates";
 import { loadTemplateWithTokenHash } from "@/lib/templates";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { webkit } from "playwright";
+
+import edgeChromium from "chrome-aws-lambda";
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from "puppeteer-core";
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 const VIEWPORT_FOR_TEMPLATE = {
   [Template.Fidenza]: { width: 1000, height: 1200 },
@@ -16,16 +28,31 @@ export default async function handler(
 
   const html = await loadTemplateWithTokenHash(template as Template, hash);
 
-  const browser = await webkit.launch();
+  const executablePath =
+    (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
+
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  });
+
   const page = await browser.newPage();
 
-  await page.setViewportSize(VIEWPORT_FOR_TEMPLATE[template as Template]);
+  await page.setViewport(VIEWPORT_FOR_TEMPLATE[template as Template]);
   await page.setContent(html);
 
   const content = await page.$("body");
   if (!content) return res.status(500).end();
 
-  const imageBuffer = await content.screenshot({ omitBackground: true });
+  const imageBuffer = await content.screenshot({
+    omitBackground: true,
+    type: "png",
+  });
+
+  await browser.close();
+
+  res.setHeader("Content-Type", "image/png");
   res.setHeader(
     "Content-Disposition",
     `inline; filename="tokenhash-${hash}-${template}.png"`
